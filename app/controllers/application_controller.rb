f64866008466282455269ea51e8858f07ad8fb9f
Vagner Oliveira <ti.vagner@gmail.com>
2020-03-17 23:29:22 -0300

    paper_trail listing all informations  ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user! 
+  before_action :set_paper_trail_whodunnit
+  before_action :set_last_seen_at, if: proc { user_signed_in? }
+
+  
+  
+
 
   def user_for_paper_trail
     # Save the user responsible for the action
-    user_signed_in? ? current_user.email : 'Guest'
+    user_signed_in? ? current_user.id : 'Guest'
   end
-  
+
+
+
+private
+  def set_last_seen_at
+    current_user.update_attribute(:last_seen_at, Time.current)
+  end
 end
